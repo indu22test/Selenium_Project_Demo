b84d6a50140999e2c985e53f39793f959d632132
vivie <vivie@192.168.2.16>
2023-09-24 11:23:50 -0400

    I am committing a corrected register.feature file and a new register.java file

---
+++ src/test/java/stepdefinitions/register.java
@@ -0,0 +1,222 @@
+package stepdefinitions;
+
+import java.time.Duration;
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class register {
+	
+	public WebDriver driver;
+
+	 String URL = "https://dsportalapp.herokuapp.com/";
+
+	 By getstarted = By.xpath("//button[text()='Get Started']");
+	 By register = By.xpath("//a[text()=' Register ']");
+	 By registerBtn = By.xpath("//input[@value='Register']");
+	 By username = By.name("username");
+	 By password = By.name("password1");
+	 By password2 = By.name("password2");
+	 By errorMessage = By.xpath("//div[@class='alert alert-primary']");
+	
+	 
+	 //background  
+	@Given("user opens DSportal app")
+	public void user_opens_d_sportal_app() {
+		System.getProperty("C:\\Users\\sheet\\eclipse-workspace\\Selenium_Project_01\\src\\test\\resources\\drivers");
+		   driver = new ChromeDriver();
+		   driver.get(URL);
+		   driver.manage().window().maximize();
+		   driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		   
+	}
+
+	@Given("user clicks get started button")
+	public void user_clicks_get_started_button() {
+		driver.findElement(getstarted).click();
+	}
+
+	@When("user clicks registration button")
+	public void user_clicks_registration_button() {
+		driver.findElement(register).click();
+	}
+
+	@Then("user navigates to register page")
+	public void user_navigates_to_register_page() {
+	}
+
+	//@TC_reg_01
+	@When("user clicks register button")
+	public void user_clicks_register_button() {
+		driver.findElement(registerBtn).click();
+	}
+
+	@Then("user should get an error message below username textbox")
+	public void user_should_get_an_error_message_below_username_textbox() {
+		String message = driver.findElement(username).getAttribute("validationMessage"); 
+		System.out.println(message);
+	}
+
+	//@TC_reg_02
+	@Then("user enters valid username")
+	public void user_enters_valid_username() {
+		driver.findElement(username).sendKeys("Jane");
+	}
+
+	@When("user clicks button")
+	public void user_clicks_button() {
+		driver.findElement(registerBtn).click();
+	}
+
+	@Then("user should get an error message below password textbox")
+	public void user_should_get_an_error_message_below_password_textbox() {
+		String message = driver.findElement(username).getAttribute("validationMessage"); 
+		System.out.println(message);
+	}
+
+	//@TC_reg_03
+	@Then("the user enters valid username")
+	public void the_user_enters_valid_username() {
+		driver.findElement(username).sendKeys("Mich");
+	}
+
+	@Then("user enters valid password")
+	public void user_enters_valid_password() {
+		driver.findElement(password).sendKeys("ilove134");
+	}
+
+	@When("the user clicks register button")
+	public void the_user_clicks_register_button() {
+		driver.findElement(registerBtn).click();
+	}
+
+	@Then("user should get an error message below password confirmation box")
+	public void user_should_get_an_error_message_below_password_confirmation_box() {
+		String message = driver.findElement(username).getAttribute("validationMessage"); 
+		System.out.println(message);
+	}
+	
+	//@TC_reg_04
+	@Then("user enters symbols as username")
+	public void user_enters_symbols_as_username() {
+		driver.findElement(username).sendKeys("@/./+/-/_");
+	}
+	
+	@Then("the user enters valid password and matching confirmatory password")
+	public void the_user_enters_valid_password_and_matching_confirmatory_password() {
+		driver.findElement(password).sendKeys("help2010");
+		driver.findElement(password2).sendKeys("help2010");
+	}
+
+	@When("the user clicks registration button")
+	public void the_user_clicks_registration_button() {
+		driver.findElement(registerBtn).click();
+	}
+
+	@Then("user should get an error message")
+	public void user_should_get_an_error_message() {
+		WebElement displayErrorMessage = driver.findElement(errorMessage);
+		System.out.println(displayErrorMessage.getText());
+	}
+
+	//@TC_reg_05
+	@Then("user enter valid username")
+	public void user_enter_valid_username() {
+		driver.findElement(username).sendKeys("Janet");
+	}
+
+	@Then("user enters valid password and non-matching password")
+	public void user_enters_valid_password_and_non_matching_password() {
+		driver.findElement(password).sendKeys("pass3456");
+		driver.findElement(password2).sendKeys("pass2345");
+	}
+
+	@When("user click register button")
+	public void user_click_register_button() {
+		driver.findElement(registerBtn).click();
+	}
+
+	@Then("user should get error message")
+	public void user_should_get_error_message() {
+		String printErrorMessage = driver.findElement(errorMessage).getText();
+		System.out.println(printErrorMessage);
+	}
+	
+	//@TC_reg_06
+	@Then("the user enter valid username")
+	public void the_user_enter_valid_username() {
+		driver.findElement(username).sendKeys("Candy");
+	}
+
+	@Then("user enters password and confirmation password less than {int} characters")
+	public void user_enters_password_and_confirmation_password_less_than_characters(Integer int1) {
+		driver.findElement(password).sendKeys("gate");
+		driver.findElement(password2).sendKeys("gate");
+	}
+
+	@When("user click registration button")
+	public void user_click_registration_button() {
+		driver.findElement(registerBtn).click();
+	}
+
+	@Then("the user should get an error message")
+	public void the_user_should_get_an_error_message() {
+		String printErrorMessage = driver.findElement(errorMessage).getText();
+		System.out.println(printErrorMessage);
+	}
+
+	//@TC_reg_07
+	@Then("user enters username")
+	public void user_enters_username() {
+		driver.findElement(username).sendKeys("Nicole");
+	}
+
+	@Then("user enters numerical password and confirmatory password")
+	public void user_enters_numerical_password_and_confirmatory_password() {
+		driver.findElement(password).sendKeys("12345678");
+		driver.findElement(password2).sendKeys("12345678");
+	}
+
+	@When("user click button")
+	public void user_click_button() {
+		driver.findElement(registerBtn).click();
+	}
+
+	@Then("user should get an error msg")
+	public void user_should_get_an_error_msg() {
+		String printErrorMessage = driver.findElement(errorMessage).getText();
+		System.out.println(printErrorMessage);
+	}
+	
+	//@TC_reg_08
+	@Then("the user enters username")
+	public void the_user_enters_username() {
+		driver.findElement(username).sendKeys("Trent");
+	}
+
+	@Then("user enters valid password and matching confirmatory password")
+	public void user_enters_valid_password_and_matching_confirmatory_password() {
+		driver.findElement(password).sendKeys("ilovecodes");
+		driver.findElement(password2).sendKeys("ilovecodes");
+	}
+
+	@When("the user click button")
+	public void the_user_click_button() {
+		driver.findElement(registerBtn).click();
+	}
+
+	@Then("user should be redirected to homepage")
+	public void user_should_be_redirected_to_homepage() {
+	}
+
+
+
+
+}
--- src/test/resources/features/register.feature
+++ src/test/resources/features/register.feature
@@ -1,55 +1,61 @@
-@Register
+Feature: Registration page functionality
+  I want to test the functionality of the registration page
 
-Feature: register
-  
- @TC_reg_01
- Scenario Outline: User registration
-    Given The user opens Register Page
-    When The user clicks "Register" button with all fields empty
-    Then It should display an error "Please fill out this field" below Username textbox
+  Background: user navigate to registration page
+    Given user opens DSportal app
+    And user clicks get started button
+    When user clicks registration button
+    Then user navigates to register page
+    
+	@TC_reg_01
+  Scenario: check if user can successfully register without filling fields
+    When user clicks register button
+    Then user should get an error message below username textbox
 
-@TC_reg_02
-Scenario Outline: User registration
-    Given The user opens Register Page
-    When The user clicks "Register" button after entering Username with other fields empty
-    Then It should display an error "Please fill out this field" below Password textbox
+	@TC_reg_02
+  Scenario: check if user can register successfully with only username
+    And user enters valid username
+    When user clicks button
+    Then user should get an error message below password textbox
 
-@TC_reg_03
-Scenario Outline: User registration
-    Given The user opens Register Page
-    When The user clicks "Register" button after entering Username and password with Password Confirmation field empty
-    Then It should display an error "Please fill out this field" below Password Confirmation textbox
+	@TC_reg_03
+  Scenario: check if user can register successfully when only confirmatory password field is blank
+    And the user enters valid username
+    And user enters valid password
+    When the user clicks register button
+    Then user should get an error message below password confirmation box
 
-@TC_reg_04
-Scenario Outline: User registration
-    Given The user opens Register Page
-    When The user enters a name with characters other than Letters, digits and @/./+/-/_
-    Then It should display an error message "Please enter a valid username"
-   
-@TC_reg_05
-Scenario Outline: User registration
-    Given The user opens Register Page
-    When The user clicks "Register" button after entering different passwords in Password and Password Confirmation fields 
-    Then It should display an error message "password_mismatch:The two password fields didn’t match."
-   
-@TC_reg_06
-Scenario Outline: User registration
-    Given The user opens Register Page
-    When The user enters a Password with characters less than 8
-    Then It should display an error message "Password should contain atleast 8 characters"
-  
-@TC_reg_07
-Scenario Outline: User registration
-    Given The user opens Register Page
-    When The user enters a Password with only numbers
-    Then  It should display an error message "Password cannot be entirely numeric"
-    
-@TC_reg_08
-Scenario Outline: User registration
-    Given The user opens Register Page
-    When The user enters username, matching password and password confirmation and clicks the register button
-    Then The user should be redirected to Homepage with the message "New Account Created. You are logged in as <username>"
-   
-    
-    
-    
+	@TC_reg_04
+  Scenario: check if user can register successfully when username has only symbols
+    And user enters symbols as username
+    And the user enters valid password and matching confirmatory password
+    When the user clicks registration button
+    Then user should get an error message
+	
+	@TC_reg_05
+  Scenario: check is user can register successfully with different passwords
+    And user enter valid username
+    And user enters valid password and non-matching password
+    When user click register button
+    Then user should get error message
+
+	@TC_reg_06
+  Scenario: check if user can register successfully with less than 8 characters
+    And the user enter valid username
+    And user enters password and confirmation password less than 8 characters
+    When user click registration button
+    Then the user should get an error message
+
+	@TC_reg_07
+  Scenario: check if user can register successfully with numerical password
+    And user enters username
+    And user enters numerical password and confirmatory password
+    When user click button
+    Then user should get an error msg
+
+	@TC_reg_08
+  Scenario: user successfully registers
+    And the user enters username
+    And user enters valid password and matching confirmatory password
+    When the user click button
+    Then user should be redirected to homepage
